1327813c4f70e2ad936619222c968afa4500b3ba
IF007 <565985999@qq.com>
2021-04-16 20:31:33 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Main {
+
+    static void swap(int[] arr,int i,int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+
+
+    }
+}
